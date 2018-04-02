eda0e51d7f826f6aef36c7ea9f66adb7d9ee5f75
Chris Chasm <chris@chasm.solutions>
2018-04-02 16:24:08 -0600

    Adding metaboxes

--- includes/dt-hooks.php
+++ includes/dt-hooks.php
@@ -46,7 +46,89 @@ abstract class DT_Zume_DT_Hook_Base
  */
 class DT_Zume_DT_Hook_User extends DT_Zume_DT_Hook_Base {
 
+    public function user_detail_box( $section ) {
+        if ( $section == 'zume_contact_details') :
+            global $post;
+            // trigger refreshed collection of data
+            DT_Zume_DT::check_user_record_update( $post->ID );
+
+            // check last_cache date
+
+            // remote_get check sum
+
+            // transfer and save new cache version
+
+            $record = get_post_meta( $post->ID, 'zume_raw_record', true );
+            ?>
+            <label class="section-header"><?php esc_html_e('Zúme Group Info' ) ?></label>
+
+            <ul class="tabs" data-tabs id="zume-tabs">
+                <li class="tabs-title is-active"><a href="#sessions" aria-selected="true"><?php esc_html_e( 'Sessions' ) ?></a></li>
+                <li class="tabs-title"><a href="#map" data-tabs-target="map"><?php esc_html_e( 'Map' ) ?></a></li>
+                <li class="tabs-title"><a data-tabs-target="raw" href="#raw"><?php esc_html_e( 'Raw' ) ?></a></li>
+            </ul>
+
+            <div class="tabs-content" data-tabs-content="zume-tabs">
+                <!-- Sessions Tab -->
+                <div class="tabs-panel is-active" id="sessions">
+
+                </div>
+
+                <!-- Map Tab-->
+                <div class="tabs-panel" id="map">
+                    <?php
+//                    $lng = $record['lng'] ?: ( $record['ip_lng'] ?: '');
+//                    $lat = $record['lat'] ?: ( $record['ip_lat'] ?: '');
+                    $address = $record['zume_user_address'] ?: $record['zume_address_from_ip'];
+                    $source = $record['zume_user_address'] ? 'User Provided Address' : 'Location from IP Address';
+                    ?>
+                    <p><?php echo esc_html( $address ) ?> <span class="text-small grey">( <?php echo esc_html( $source ) ?> )</span></p>
+<!--                    <a id="map-reveal" data-open="--><?php //echo md5( $address ?? 'none' ) ?><!--"><img src="https://maps.googleapis.com/maps/api/staticmap?center=--><?php //echo esc_attr( $lat ) . ',' . esc_attr( $lng  ) ?><!--&zoom=6&size=640x640&scale=1&markers=color:red|--><?php //echo esc_attr( $lat  ) . ',' . esc_attr( $lng ) ?><!--&key=--><?php //echo esc_attr( Disciple_Tools_Google_Geocode_API::$key ); ?><!--"/></a>-->
+                    <p class="center"><a data-open="<?php echo md5( $address ?? 'none' ) ?>"><?php esc_html_e('click to show large map') ?></a></p>
+
+                    <div class="reveal large" id="<?php echo md5( $address ?? 'none' ) ?>" data-reveal>
+<!--                        <img  src="https://maps.googleapis.com/maps/api/staticmap?center=--><?php //echo esc_attr( $lat ) . ',' . esc_attr( $lng ) ?><!--&zoom=5&size=640x640&scale=2&markers=color:red|--><?php //echo esc_attr( $lat ) . ',' . esc_attr( $lng ) ?><!--&key=--><?php //echo esc_attr( Disciple_Tools_Google_Geocode_API::$key ); ?><!--"/>-->
+                        <button class="close-button" data-close aria-label="Close modal" type="button">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Raw Tab-->
+                <div class="tabs-panel" id="raw" style="width: 100%;height: 300px;overflow-y: scroll;overflow-x:hidden;">
+                    <?php
+                    if ( $record ) {
+                        foreach ( $record as $key => $value ) {
+                            echo '<strong>' . $key . ': </strong>' . maybe_serialize( $value ) . '<br>';
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+
+
+            <script>
+                jQuery(document).ready(function(){
+                    jQuery.ajax(window.location, function(data) {
+                        jQuery('#map-reveal').html(data).foundation();
+                    });
+                })
+            </script>
+        <?php
+        endif;
+
+    }
+
+    public function user_filter_box( $sections, $post_type = '' ) {
+        if ($post_type === "contacts") {
+            $sections[] = 'zume_contact_details';
+        }
+        return $sections;
+    }
+
     public function __construct() {
+        add_action('dt_details_additional_section', [$this, 'user_detail_box'] );
+        add_filter( 'dt_details_additional_section_ids', [$this, 'user_filter_box'], 999, 2 );
 
         parent::__construct();
     }
@@ -58,8 +140,103 @@ class DT_Zume_DT_Hook_User extends DT_Zume_DT_Hook_Base {
  */
 class DT_Zume_DT_Hook_Groups extends DT_Zume_DT_Hook_Base {
 
+    public function group_detail_box( $section ) {
+        if ( $section == 'zume_group_details') :
+            // trigger refreshed collection of data
+
+            // check last_cache date
+
+            // remote_get check sum
+
+            // transfer and save new cache version
+
+            global $post;
+            $record = get_post_meta( $post->ID, 'zume_raw_record', true );
+            ?>
+            <label class="section-header"><?php esc_html_e('Zúme Group Info' ) ?></label>
+
+            <ul class="tabs" data-tabs id="zume-tabs">
+                <li class="tabs-title is-active"><a href="#sessions" aria-selected="true"><?php esc_html_e( 'Sessions' ) ?></a></li>
+                <li class="tabs-title"><a href="#map" data-tabs-target="map"><?php esc_html_e( 'Map' ) ?></a></li>
+                <li class="tabs-title"><a data-tabs-target="raw" href="#raw"><?php esc_html_e( 'Raw' ) ?></a></li>
+            </ul>
+
+            <div class="tabs-content" data-tabs-content="zume-tabs">
+                <!-- Sessions Tab -->
+                <div class="tabs-panel is-active" id="sessions">
+                    <?php
+                    if ( $record ) { ?>
+
+                        <!-- sessions -->
+                        <button class="button <?php echo esc_html( $record['session_1'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 1' ) ?></button>
+                        <button class="button <?php echo esc_html( $record['session_2'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 2' ) ?></button>
+                        <button class="button <?php echo esc_html( $record['session_3'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 3' ) ?></button>
+                        <button class="button <?php echo esc_html( $record['session_4'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 4' ) ?></button>
+                        <button class="button <?php echo esc_html( $record['session_5'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 5' ) ?></button>
+                        <button class="button <?php echo esc_html( $record['session_6'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 6' ) ?></button>
+                        <button class="button <?php echo esc_html( $record['session_7'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 7' ) ?></button>
+                        <button class="button <?php echo esc_html( $record['session_8'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 8' ) ?></button>
+                        <button class="button <?php echo esc_html( $record['session_9'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 9' ) ?></button>
+                        <button class="button <?php echo esc_html( $record['session_10'] ? 'success' : 'hollow' ) ?> expanded" type="button"><?php echo esc_html('Session 10' ) ?></button>
+
+                    <?php } // endif ?>
+                </div>
+
+                <!-- Map Tab-->
+                <div class="tabs-panel" id="map">
+                    <?php
+                    $lng = $record['lng'] ?: $record['ip_lng'];
+                    $lat = $record['lat'] ?: $record['ip_lat'];
+                    $address = $record['address'] ?: $record['ip_address'];
+                    $source = $record['address'] ? 'User Provided Address' : 'Location from IP Address';
+                    ?>
+                    <p><?php echo esc_html( $address ) ?> <span class="text-small grey">( <?php echo esc_html( $source ) ?> )</span></p>
+                    <a id="map-reveal" data-open="<?php echo md5( $address ?? 'none' ) ?>"><img src="https://maps.googleapis.com/maps/api/staticmap?center=<?php echo esc_attr( $lat ) . ',' . esc_attr( $lng  ) ?>&zoom=6&size=640x640&scale=1&markers=color:red|<?php echo esc_attr( $lat  ) . ',' . esc_attr( $lng ) ?>&key=<?php echo esc_attr( Disciple_Tools_Google_Geocode_API::$key ); ?>"/></a>
+                    <p class="center"><a data-open="<?php echo md5( $address ?? 'none' ) ?>"><?php esc_html_e('click to show large map') ?></a></p>
+
+                    <div class="reveal large" id="<?php echo md5( $address ?? 'none' ) ?>" data-reveal>
+                        <img  src="https://maps.googleapis.com/maps/api/staticmap?center=<?php echo esc_attr( $lat ) . ',' . esc_attr( $lng ) ?>&zoom=5&size=640x640&scale=2&markers=color:red|<?php echo esc_attr( $lat ) . ',' . esc_attr( $lng ) ?>&key=<?php echo esc_attr( Disciple_Tools_Google_Geocode_API::$key ); ?>"/>
+                        <button class="close-button" data-close aria-label="Close modal" type="button">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Raw Tab-->
+                <div class="tabs-panel" id="raw" style="width: 100%;height: 300px;overflow-y: scroll;overflow-x:hidden;">
+                    <?php
+                    if ( $record ) {
+                        foreach ( $record as $key => $value ) {
+                            echo '<strong>' . $key . ': </strong>' . maybe_serialize( $value ) . '<br>';
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+
+
+            <script>
+                jQuery(document).ready(function(){
+                    jQuery.ajax(window.location, function(data) {
+                        jQuery('#map-reveal').html(data).foundation();
+                    });
+                })
+            </script>
+            <?php
+        endif;
+
+    }
+
+    public function groups_filter_box( $sections, $post_type = '' ) {
+        if ($post_type === "groups") {
+            $sections[] = 'zume_group_details';
+        }
+        return $sections;
+    }
 
     public function __construct() {
+        add_action('dt_details_additional_section', [$this, 'group_detail_box'] );
+        add_filter( 'dt_details_additional_section_ids', [$this, 'groups_filter_box'], 999, 2 );
 
         parent::__construct();
     }
--- includes/dt.php
+++ includes/dt.php
@@ -42,4 +42,119 @@ class DT_Zume_DT
 
         return $post_id;
     }
+
+    public static function check_user_record_update( $contact_id ) {
+        dt_write_log( __METHOD__ );
+        dt_write_log( $contact_id );
+
+        // check if already checked today
+        if ( self::test_last_check( $contact_id ) ) {
+            return true;
+        }
+
+        $check_sum = get_post_meta( $contact_id, 'zume_check_sum', true );
+
+        $foreign_key = get_post_meta( $contact_id, 'zume_foreign_key', true );
+
+        $site = self::get_site_details( get_option('zume_default_site') );
+
+        // Send remote request
+        $args = [
+        'method' => 'POST',
+        'body' => [
+            'transfer_token' => $site['transfer_token'],
+            'zume_check_sum' => $check_sum,
+            'zume_foreign_key' => $foreign_key,
+            ]
+        ];
+        $result = self::remote_send( 'check_user_record_update', $site['url'], $args );
+        if ( isset( $result['body'] ) ) {
+            $response = json_decode( $result['body'], true );
+            if ( isset( $response['status'] ) ) {
+                if ( $response['status'] == 'OK' ) {
+                    // no update needed
+                    dt_write_log( $response['status'] );
+                    update_post_meta( $contact_id, 'zume_last_check', current_time('mysql' ) );
+                } elseif ( $response['status'] == 'Update_Needed' && isset( $response['raw_record'] ) ) {
+                    // updated needed
+                    $new_check_sum = $response['raw_record']['zume_check_sum'] ?? $check_sum;
+
+                    update_post_meta( $contact_id, 'zume_check_sum', $new_check_sum );
+                    update_post_meta( $contact_id, 'zume_raw_record', $response['raw_record'] );
+                    update_post_meta( $contact_id, 'zume_last_check', current_time('mysql' ) );
+                } else {
+                    // error
+                    dt_write_log( 'Contact update error.' );
+                    dt_write_log( $response );
+                }
+            } else {
+                // error
+                dt_write_log( $response );
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static function test_last_check( $post_id ) : bool {
+        $timestamp = get_post_meta( $post_id, 'zume_last_check', true );
+        if ( date('Ymd') > date('Ymd', strtotime($timestamp) ) ) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Get the token and url of the site
+     *
+     * @param $site_key
+     *
+     * @return array
+     */
+    public static function get_site_details( $site_key ) {
+        $keys = Site_Link_System::get_site_keys();
+
+        $site1 = $keys[$site_key]['site1'];
+        $site2 = $keys[$site_key]['site2'];
+
+        $url = Site_Link_System::get_non_local_site( $site1, $site2 );
+        $transfer_token = Site_Link_System::create_transfer_token_for_site( $site_key );
+
+        return [
+        'url' => $url,
+        'transfer_token' => $transfer_token
+        ];
+    }
+
+    /**
+     * Send Post Request
+     * $args = [
+     *  'method' => 'POST',
+     *   'body' => [
+     *   'transfer_token' => $site['transfer_token'],
+     *   'transfer_record' => $fields,
+     *   'zume_foreign_key' => $user_data['zume_foreign_key'],
+     *   'zume_language' => $user_data['zume_language'],
+     *   'zume_check_sum' => $user_data['zume_check_sum'],
+     * ]
+     * ];
+     *
+     * @param $endpoint
+     * @param $url
+     * @param $args
+     *
+     * @return array|\WP_Error
+     */
+    public static function remote_send( $endpoint, $url, $args ) {
+
+        $result = wp_remote_post( 'https://' . $url . '/wp-json/dt-public/v1/zume/' . $endpoint, $args );
+
+        if ( is_wp_error( $result ) ) {
+            return new WP_Error( 'failed_remote_get', $result->get_error_message() );
+        }
+        return $result;
+    }
 }
+
+
